78a0a470e18eae28ae6f688decbdb18406abc4fa
Sushil Kumar <sushil@smarttraffic.in>
2026-09-14 20:46:16 +0530

    Make featured image circular and remove yellow background

--- src/app/core/data/drawings.generated.ts
+++ src/app/core/data/drawings.generated.ts
@@ -1,99 +1,515 @@
 // Generated automatically. Do not edit directly.
 export const localDrawings = [
   {
-    "title": "A (6)",
+    "title": "SK9129866",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (6).jpeg",
-    "alt": "A (6)"
+    "image": "/drawings/ColorArt/2026-09-14/SK9129866.jpeg",
+    "alt": "SK9129866"
   },
   {
-    "title": "A (5)",
+    "title": "SK5856512",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (5).jpeg",
-    "alt": "A (5)"
+    "image": "/drawings/ColorArt/2026-09-14/SK5856512.jpeg",
+    "alt": "SK5856512"
   },
   {
-    "title": "A (4)",
+    "title": "SK32015407",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (4).jpeg",
-    "alt": "A (4)"
+    "image": "/drawings/ColorArt/2026-09-14/SK32015407.jpeg",
+    "alt": "SK32015407"
   },
   {
-    "title": "A (3)",
+    "title": "SK30350945",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (3).jpeg",
-    "alt": "A (3)"
+    "image": "/drawings/ColorArt/2026-09-14/SK30350945.jpeg",
+    "alt": "SK30350945"
   },
   {
-    "title": "A (2)",
+    "title": "SK28202843",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (2).jpeg",
-    "alt": "A (2)"
+    "image": "/drawings/ColorArt/2026-09-14/SK28202843.jpeg",
+    "alt": "SK28202843"
   },
   {
-    "title": "A (1)",
+    "title": "SK2813644",
     "category": "Color Art",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/ColorArt/2026-09-14/a (1).jpeg",
-    "alt": "A (1)"
+    "image": "/drawings/ColorArt/2026-09-14/SK2813644.jpeg",
+    "alt": "SK2813644"
   },
   {
-    "title": "Images",
+    "title": "SK2622872",
+    "category": "Color Art",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/ColorArt/2026-09-14/SK2622872.jpeg",
+    "alt": "SK2622872"
+  },
+  {
+    "title": "SK876366",
     "category": "Pencil Sketches",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/PencilSketches/2026-09-14/images.jpg",
-    "alt": "Images"
+    "image": "/drawings/PencilSketches/2026-09-14/SK876366.jpeg",
+    "alt": "SK876366"
   },
   {
-    "title": "A (3)",
+    "title": "SK7455370",
     "category": "Pencil Sketches",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/PencilSketches/2026-09-14/a (3).jpeg",
-    "alt": "A (3)"
+    "image": "/drawings/PencilSketches/2026-09-14/SK7455370.jpeg",
+    "alt": "SK7455370"
   },
   {
-    "title": "A (2)",
+    "title": "SK6928221",
     "category": "Pencil Sketches",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/PencilSketches/2026-09-14/a (2).jpeg",
-    "alt": "A (2)"
+    "image": "/drawings/PencilSketches/2026-09-14/SK6928221.jpeg",
+    "alt": "SK6928221"
   },
   {
-    "title": "A (1)",
+    "title": "SK6644826",
     "category": "Pencil Sketches",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/PencilSketches/2026-09-14/a (1).jpeg",
-    "alt": "A (1)"
+    "image": "/drawings/PencilSketches/2026-09-14/SK6644826.jpeg",
+    "alt": "SK6644826"
   },
   {
-    "title": "A (1)",
+    "title": "SK636494",
     "category": "Pencil Sketches",
     "year": "2026",
     "date": "2026-09-14",
-    "image": "/drawings/PencilSketches/2026-09-14/a (1).avif",
-    "alt": "A (1)"
+    "image": "/drawings/PencilSketches/2026-09-14/SK636494.jpeg",
+    "alt": "SK636494"
+  },
+  {
+    "title": "SK5075155",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK5075155.jpeg",
+    "alt": "SK5075155"
+  },
+  {
+    "title": "SK5008160",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK5008160.jpeg",
+    "alt": "SK5008160"
+  },
+  {
+    "title": "SK4802454",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK4802454.jpeg",
+    "alt": "SK4802454"
+  },
+  {
+    "title": "SK3774174",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK3774174.jpeg",
+    "alt": "SK3774174"
+  },
+  {
+    "title": "SK32793732",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK32793732.jpeg",
+    "alt": "SK32793732"
+  },
+  {
+    "title": "SK32588130",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK32588130.jpeg",
+    "alt": "SK32588130"
+  },
+  {
+    "title": "SK31162365",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK31162365.jpeg",
+    "alt": "SK31162365"
+  },
+  {
+    "title": "SK31008868",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK31008868.jpeg",
+    "alt": "SK31008868"
+  },
+  {
+    "title": "SK30940842",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK30940842.jpeg",
+    "alt": "SK30940842"
+  },
+  {
+    "title": "SK30889818",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK30889818.jpeg",
+    "alt": "SK30889818"
+  },
+  {
+    "title": "SK30755536",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK30755536.jpeg",
+    "alt": "SK30755536"
+  },
+  {
+    "title": "SK30315449",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK30315449.jpeg",
+    "alt": "SK30315449"
+  },
+  {
+    "title": "SK29963574",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK29963574.jpeg",
+    "alt": "SK29963574"
+  },
+  {
+    "title": "SK29874034",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK29874034.jpeg",
+    "alt": "SK29874034"
+  },
+  {
+    "title": "SK29032182",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK29032182.jpeg",
+    "alt": "SK29032182"
+  },
+  {
+    "title": "SK2862230",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK2862230.jpeg",
+    "alt": "SK2862230"
+  },
+  {
+    "title": "SK28413650",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28413650.jpeg",
+    "alt": "SK28413650"
+  },
+  {
+    "title": "SK28184566",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28184566.jpeg",
+    "alt": "SK28184566"
+  },
+  {
+    "title": "SK28126674",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28126674.jpeg",
+    "alt": "SK28126674"
+  },
+  {
+    "title": "SK28114556",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28114556.jpeg",
+    "alt": "SK28114556"
+  },
+  {
+    "title": "SK28093632",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28093632.jpeg",
+    "alt": "SK28093632"
+  },
+  {
+    "title": "SK28082451",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28082451.jpeg",
+    "alt": "SK28082451"
+  },
+  {
+    "title": "SK28073682",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK28073682.jpeg",
+    "alt": "SK28073682"
+  },
+  {
+    "title": "SK2759377",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK2759377.jpeg",
+    "alt": "SK2759377"
+  },
+  {
+    "title": "SK27225636",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK27225636.jpeg",
+    "alt": "SK27225636"
+  },
+  {
+    "title": "SK27110427",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK27110427.jpeg",
+    "alt": "SK27110427"
+  },
+  {
+    "title": "SK26621788",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK26621788.jpeg",
+    "alt": "SK26621788"
+  },
+  {
+    "title": "SK26124661",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK26124661.jpeg",
+    "alt": "SK26124661"
+  },
+  {
+    "title": "SK25239496",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK25239496.jpeg",
+    "alt": "SK25239496"
+  },
+  {
+    "title": "SK25165721",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK25165721.jpeg",
+    "alt": "SK25165721"
+  },
+  {
+    "title": "SK24439800",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK24439800.jpeg",
+    "alt": "SK24439800"
+  },
+  {
+    "title": "SK24183567",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK24183567.jpeg",
+    "alt": "SK24183567"
+  },
+  {
+    "title": "SK22492690",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK22492690.jpeg",
+    "alt": "SK22492690"
+  },
+  {
+    "title": "SK22297970",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK22297970.jpeg",
+    "alt": "SK22297970"
+  },
+  {
+    "title": "SK21669665",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK21669665.jpeg",
+    "alt": "SK21669665"
+  },
+  {
+    "title": "SK21065650",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK21065650.jpeg",
+    "alt": "SK21065650"
+  },
+  {
+    "title": "SK20833276",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK20833276.jpeg",
+    "alt": "SK20833276"
+  },
+  {
+    "title": "SK20374778",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK20374778.jpeg",
+    "alt": "SK20374778"
+  },
+  {
+    "title": "SK18922437",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK18922437.jpeg",
+    "alt": "SK18922437"
+  },
+  {
+    "title": "SK18088546",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK18088546.jpeg",
+    "alt": "SK18088546"
+  },
+  {
+    "title": "SK17263541",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK17263541.jpeg",
+    "alt": "SK17263541"
+  },
+  {
+    "title": "SK16240129",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK16240129.jpeg",
+    "alt": "SK16240129"
+  },
+  {
+    "title": "SK15739378",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK15739378.jpeg",
+    "alt": "SK15739378"
+  },
+  {
+    "title": "SK15140478",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK15140478.jpeg",
+    "alt": "SK15140478"
+  },
+  {
+    "title": "SK15116523",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK15116523.jpeg",
+    "alt": "SK15116523"
+  },
+  {
+    "title": "SK14784937",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK14784937.jpeg",
+    "alt": "SK14784937"
+  },
+  {
+    "title": "SK1370118",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK1370118.jpeg",
+    "alt": "SK1370118"
+  },
+  {
+    "title": "SK12619738",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK12619738.jpeg",
+    "alt": "SK12619738"
+  },
+  {
+    "title": "SK12001438",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK12001438.jpeg",
+    "alt": "SK12001438"
+  },
+  {
+    "title": "SK10934226",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK10934226.jpeg",
+    "alt": "SK10934226"
+  },
+  {
+    "title": "SK10383413",
+    "category": "Pencil Sketches",
+    "year": "2026",
+    "date": "2026-09-14",
+    "image": "/drawings/PencilSketches/2026-09-14/SK10383413.jpeg",
+    "alt": "SK10383413"
   }
 ] as const;
 export const featuredDrawing = {
-  "title": "Feature",
+  "title": "Apple",
   "category": "Featured Drawing",
   "year": "",
   "date": "",
-  "image": "/drawings/FeatureDrawing/feature.jpeg",
-  "alt": "Feature"
+  "image": "/drawings/FeatureDrawing/apple.jpeg",
+  "alt": "Apple"
 } as const;
